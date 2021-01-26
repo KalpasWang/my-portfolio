e6a49eabcad4e9922056b50f7395d41f61fe0c5f
Kalpas Wang <wf6212@gmail.com>
2021-01-26 15:57:09 +0800

    修改menu與home section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,41 +1,41 @@
-var menuBtn = document.querySelector(".menu-btn");
-var menu = document.querySelector(".menu");
-var menuNav = document.querySelector(".menu-nav");
-//var menuBranding = document.querySelector(".menu-branding");
-var menuItems = document.querySelectorAll(".menu-item");
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.menu');
+var menuNav = document.querySelector('.menu-nav');
+var menuBranding = document.querySelector('.menu-branding');
+var menuItems = document.querySelectorAll('.menu-item');
 
 var showMenu = false;
 
-menuBtn.addEventListener("click", function() {
+menuBtn.addEventListener('click', function () {
   if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    //menuBranding.classList.add("show");
-    menuItems.forEach(item => item.classList.add("show"));
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    menuItems.forEach((item) => item.classList.add('show'));
     showMenu = true;
   } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    //menuBranding.classList.remove("show");
-    menuItems.forEach(item => item.classList.remove("show"));
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    menuItems.forEach((item) => item.classList.remove('show'));
     showMenu = false;
   }
 });
 
-var $bar = $(".progress-bar");
+var $bar = $('.progress-bar');
 var $barOffset = $bar.offset().top;
 var $window = $(window);
 
-$window.on("scroll", function() {
+$window.on('scroll', function () {
   var scrollPoint = $window.height() - $bar.height() + $window.scrollTop();
   //console.log(scrollPoint + " " + $barOffset);
-  if (scrollPoint > $barOffset && !$bar.hasClass("animation")) {
-    $bar.addClass("animation");
-    $(".progress").animate(
+  if (scrollPoint > $barOffset && !$bar.hasClass('animation')) {
+    $bar.addClass('animation');
+    $('.progress').animate(
       {
-        width: "100%"
+        width: '100%',
       },
       1000
     );
