a7849d3aeac2850c832fb73f63a575a74ddda719
KalpasWang <48082968+KalpasWang@users.noreply.github.com>
2021-02-10 16:59:07 +0800

    修正 scrollSpy resize 的問題與調整 css 細節

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,74 +1,85 @@
 $(function () {
   const $window = $(window);
-  var menuBtn = document.querySelector('.menu-btn');
-  var menu = document.querySelector('.menu');
-  var menuNav = document.querySelector('.menu-nav');
-  var menuItems = document.querySelectorAll('.menu-item');
-  var showMenu = false;
+  const $menuBtn = $('.menu-btn');
+  const $menuGroup = $('.menu, .menu-nav, .menu-item');
+  const $spyingAreas = $('#home, #about, #experience, #skills, #works');
+  const $animatingAreas = $('.time-item, .skills-item, .project');
+  let showMenu = false;
 
-  menuBtn.addEventListener('click', function () {
+  // 加上選單按鈕與 X 切換效果
+  $menuBtn.click(function () {
     if (!showMenu) {
-      menuBtn.classList.add('close');
-      menu.classList.add('show');
-      menuNav.classList.add('show');
-      menuItems.forEach((item) => item.classList.add('show'));
+      $menuBtn.addClass('close');
+      $menuGroup.addClass('show');
       showMenu = true;
     } else {
-      menuBtn.classList.remove('close');
-      menu.classList.remove('show');
-      menuNav.classList.remove('show');
-      menuItems.forEach((item) => item.classList.remove('show'));
+      $menuBtn.removeClass('close');
+      $menuGroup.removeClass('show');
       showMenu = false;
     }
   });
 
-  // scrollSpy - 偵測捲軸位置並且自動切換目前所在區域的選單選項
+  // scrollSpy - 偵測捲軸位置，這裡有兩個功能需要用到 scrollSpy
+  // 一個是自動切換目前畫面上的區域所相對應的選單選項
+  // 一個是設定滑動到要出現動畫的元素
+  function addScrollSpy(target, extraHeight, isSpyingAreas) {
+    target.each(function () {
+      const $this = $(this);
+      const offset = $this.offset();
+      const id = $this.attr('id');
 
-  // function addScrollSpy(extraHeight)
+      const min = id === 'home' ? 0 : offset.top - extraHeight;
+      const max =
+        id === 'works'
+          ? $(document).height()
+          : offset.top + $this.height() - extraHeight;
 
-  $('#home, #about, #experience, #skills, #works').each(function () {
-    var $this = $(this);
-    const $window = $(window);
-    var position = $this.offset();
-    const id = $this.attr('id');
+      // 如果對象是 spyingAreas
+      if (isSpyingAreas) {
+        // 計算剛載入頁面時要 highlight 的 menu item
+        $menuGroup.removeClass('current');
+        if ($window.scrollTop() >= min /* &&  $window.scrollTop() <= max */) {
+          $('#' + $this.data('menu')).addClass('current');
+        }
+      } else {
+        // 當卷軸位置小於 min，才隱藏要動畫的對象
+        $this.removeClass('hide');
+        if ($window.scrollTop() < min) {
+          $this.addClass('hide');
+        }
+      }
 
-    // 計算剛載入頁面時要 highlight 的 menu item
-    const extraHeight = $window.height() / 3;
-    const min = id === 'home' ? 0 : position.top - extraHeight;
-    const max =
-      id === 'works'
-        ? $(document).height()
-        : position.top + $this.height() - extraHeight;
-
-    if ($window.scrollTop() >= min && $window.scrollTop() <= max) {
-      $('#' + $this.data('menu')).addClass('current');
-    }
-
-    // 偵測是否接觸到邊界，當進入該區域就切換 current 位置
-    $this.scrollspy({
-      min: (function () {
-        const extraHeight = $window.height() / 3;
-        return id === 'home' ? 0 : position.top - extraHeight;
-      })(),
-      max: (function () {
-        const extraHeight = $window.height() / 3;
-        if (id === 'works') return $(document).height();
-        return position.top + $this.height() - extraHeight;
-      })(),
-      onEnter: function onEnter(element) {
-        const current = $(element).data('menu');
-        // console.log($(element).data('menu'));
-        $('.menu-item').removeClass('current');
-        $('#' + current).addClass('current');
-      },
+      $this.scrollspy({
+        min: min,
+        max: max,
+        onEnter: function (element) {
+          if (isSpyingAreas) {
+            // 偵測是否接觸到邊界，當進入該區域就切換 current 位置
+            const current = $(element).data('menu');
+            $('.menu-item').removeClass('current');
+            $('#' + current).addClass('current');
+          } else {
+            // 進入就移除 hide 開始動畫
+            $(element).removeClass('hide');
+          }
+        },
+      });
     });
-  });
+  }
 
-  // addScrollSpy()
-  // $window.resize(function () {
-  //   destroyScrollSpy()
-  //   addScrollSpy()
-  // })
+  function destroyScrollSpy() {
+    $window.scrollspy({}, 'destroy');
+  }
+
+  //
+  addScrollSpy($spyingAreas, $window.height() / 3, true);
+  addScrollSpy($animatingAreas, $window.height() * 0.8, false);
+
+  $window.resize(function () {
+    destroyScrollSpy();
+    addScrollSpy($spyingAreas, $window.height() / 3, true);
+    addScrollSpy($animatingAreas, $window.height() * 0.8, false);
+  });
 
   // 按下 menu link 後滑動到相對應的位置
   $('.menu-nav-link').click(function (e) {
@@ -80,25 +91,5 @@ $(function () {
       },
       500
     );
-  });
-
-  // 設定滑動到要出現動畫的元素
-  $('.time-item, .skills-item, .project').each(function () {
-    const $this = $(this);
-    const offset = $this.offset();
-    const extraHeight = $window.height() * 0.8;
-
-    if ($window.scrollTop() < offset.top - extraHeight) {
-      $this.addClass('hide');
-    }
-
-    $this.scrollspy({
-      min: offset.top - extraHeight,
-      max: offset.top + $this.height() - extraHeight,
-      onEnter(el) {
-        $el = $(el);
-        $el.removeClass('hide');
-      },
-    });
   });
 });
